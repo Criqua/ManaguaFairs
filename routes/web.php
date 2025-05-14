269836107ee710a1837d8e790974c163ce3a3510
Cristian <cj83774932@gmail.com>
2025-05-14 12:13:33 -0600

    Minor design tweaks and add stub test routes

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Stub para vistas de ferias y emprendedores
+Route::view('/fairs', 'fairs.index')
+     ->name('fairs.index');
+Route::view('/fairs/create', 'fairs.create')
+     ->name('fairs.create');
+Route::view('/fairs/edit', 'fairs.edit')
+     ->name('fairs.edit');
+
+Route::view('/entrepreneurs', 'entrepreneurs.index')
+     ->name('entrepreneurs.index');
+Route::view('/entrepreneurs/create', 'entrepreneurs.create')
+     ->name('entrepreneurs.create');
+Route::view('/entrepreneurs/edit', 'entrepreneurs.edit')
+     ->name('entrepreneurs.edit');
 
 require __DIR__.'/auth.php';
